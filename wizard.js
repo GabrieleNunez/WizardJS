b64ab5f3ddda18f68685b1118585816db28dbdea
GabrieleNunez <gabe@colossalventures.com>
2014-10-02 02:20:08 -0400

    Foundations laid

--- wizard.js
+++ wizard.js
@@ -1,5 +1,20 @@
 (function($){
 	$.fn.wizard = function(options){
-		
+		var options = options;
+		if(options == undefined || options == null){
+			options = $.__wizardDefault();
+		}		
+		var pages = $(this).find(".page");
+		//hide all pages if not already
+		if(options.autoHide){
+			$(pages).css("display","none");
+		}
+	}
+	$.__wizardDefault = function(){
+		return {
+					alwaysOnTop : true,
+					autoHide : true,
+					postForm : true,
+			   };
 	}
 })(jQuery);
